62e2f5768b2ba6b80fa0c788fb7ce5e42912a3cf
Ricky DeFazio <36946678+RickyDeFazio@users.noreply.github.com>
2019-04-11 16:21:50 -0400

    Update

--- js/Game.js
+++ js/Game.js
@@ -1,3 +1,22 @@
 /* Treehouse FSJS Techdegree
  * Project 4 - OOP Game App
  * Game.js */
+
+class Game {
+  constructor() {
+    this.missed = 0;
+    this.phrases = this.createPhrases();
+    this.activePhrase = null;
+  }
+
+  createPhrases() {
+    const phrase1 = new Phrase('Fear cuts deeper than swords');
+    const phrase2 = new Phrase('When you play a game of thones, you win or you die');
+    const phrase3 = new Phrase('Most men would rather deny a hard truth than face it');
+    const phrase4 = new Phrase('Winter is coming');
+    const phrase5 = new Phrase('Nothing burns like the cold');
+    const phraseArr = [phrase1, phrase2, phrase3, phrase4, phrase5];
+    return phraseArr;
+  }
+
+}
--- js/Phrase.js
+++ js/Phrase.js
@@ -1,3 +1,24 @@
 /* Treehouse FSJS Techdegree
  * Project 4 - OOP Game App
  * Phrase.js */
+
+class Phrase {
+  constructor(phrase) {
+    this.phrase = phrase.toLowerCase();
+  }
+
+  addPhraseToDisplay() {
+    
+  }
+
+  checkLetter(letter) {
+    if (this.phrase.includes(letter)){
+      return true;
+    }
+    return false;
+  }
+
+  showMatchedLetter() {
+
+  }
+}
