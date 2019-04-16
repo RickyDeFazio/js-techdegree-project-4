7b9d236d423f89d0898173c95dfcd17b7040f029
Ricky DeFazio <36946678+RickyDeFazio@users.noreply.github.com>
2019-04-15 21:49:49 -0400

    Step 12

--- js/app.js
+++ js/app.js
@@ -5,12 +5,31 @@
 let game = null;
 const btn_reset = document.querySelector('#btn__reset');
 
+const qwerty = document.getElementById('qwerty');
+const keyrow = document.querySelectorAll('.keyrow');
+
 btn_reset.addEventListener('click', () => {
+  const ul = document.querySelector('#phrase ul');
+  while (ul.firstChild) {
+    ul.removeChild(ul.firstChild);
+  }
+  for (let i = 0; i < keyrow.length; i++){
+    const childrenKeys = keyrow[i].children;
+    for (let i = 0; i < childrenKeys.length; i++){
+      childrenKeys[i].className = 'key';
+      childrenKeys[i].disabled = false;
+    }
+  }
+  const scoreboardOl = document.querySelector('#scoreboard ol');
+  const scoreboardLi = scoreboardOl.children;
+  for (let i = 0; i < scoreboardLi.length; i++){
+    scoreboardLi[i].firstChild.src = 'images/liveHeart.png';
+  }
   game = new Game();
   game.startGame();
 });
 
-const qwerty = document.getElementById('qwerty');
+
 qwerty.addEventListener('click', (e) => {
   if (e.target.classList.contains('key')) {
     game.handleInteraction(e.target);
